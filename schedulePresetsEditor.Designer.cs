00f7109fff48378c1602a63708230e16ab2e0621
roman78965123 <roman78965123@gmail.com>
2024-08-28 23:46:31 +0300

    Добавлена кнопка)

--- schedulePresetsEditor.Designer.cs
+++ schedulePresetsEditor.Designer.cs
@@ -28,18 +28,42 @@
         /// </summary>
         private void InitializeComponent()
         {
+            comboBox1 = new ComboBox();
+            button1 = new Button();
             SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(12, 12);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(146, 23);
+            comboBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(164, 12);
+            button1.Name = "button1";
+            button1.Size = new Size(120, 23);
+            button1.TabIndex = 1;
+            button1.Text = "Создать новое";
+            button1.UseVisualStyleBackColor = true;
             // 
             // schedulePresetsEditor
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Controls.Add(comboBox1);
             Name = "schedulePresetsEditor";
             Text = "Редактор готовых рассписаний";
             ResumeLayout(false);
         }
 
         #endregion
+
+        private ComboBox comboBox1;
+        private Button button1;
     }
 }
