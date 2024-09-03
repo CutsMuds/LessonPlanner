9db08ed364086e361c296ca7a0edb27c09b45d11
CutsMuds <home@DESKTOP-6L1KGKF>
2024-09-03 15:10:47 +0300

    test commit

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -30,15 +30,16 @@
         {
             daysTree = new TreeView();
             monthPicker = new ComboBox();
+            groupBox1 = new GroupBox();
             SuspendLayout();
             // 
             // daysTree
             // 
-            daysTree.Anchor = AnchorStyles.Top | AnchorStyles.Bottom;
+            daysTree.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
             daysTree.Location = new Point(12, 41);
             daysTree.Name = "daysTree";
             daysTree.PathSeparator = "/";
-            daysTree.Size = new Size(147, 457);
+            daysTree.Size = new Size(184, 457);
             daysTree.TabIndex = 0;
             daysTree.AfterExpand += daysTree_AfterExpand;
             // 
@@ -48,15 +49,25 @@
             monthPicker.Items.AddRange(new object[] { "1. Январь (Січень)", "2. Февраль (Лютий)", "3. Март (Березень)", "4. Апрель (Квітень)", "5. Май (Травень)", "6. Июнь (Червень)", "7. Июль (Липень)", "8. Август (Серпень)", "9. Сентябрь (Вересень)", "10. Октябрь (Жовтень)", "11. Ноябрь (Листопад)", "12. Декабрь (Грудень)" });
             monthPicker.Location = new Point(12, 12);
             monthPicker.Name = "monthPicker";
-            monthPicker.Size = new Size(147, 23);
+            monthPicker.Size = new Size(184, 23);
             monthPicker.TabIndex = 1;
             monthPicker.SelectedIndexChanged += monthPicker_SelectedIndexChanged;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Location = new Point(202, 12);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(405, 486);
+            groupBox1.TabIndex = 2;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Задания";
             // 
             // MainForm
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 510);
+            ClientSize = new Size(621, 510);
+            Controls.Add(groupBox1);
             Controls.Add(daysTree);
             Controls.Add(monthPicker);
             Name = "MainForm";
@@ -69,5 +80,6 @@
 
         private TreeView daysTree;
         private ComboBox monthPicker;
+        private GroupBox groupBox1;
     }
 }
